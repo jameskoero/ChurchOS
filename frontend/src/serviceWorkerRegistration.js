f63580a272cc5d00bcc89b7525fa7fddd71e43e9
James Koero <jameskoero@users.noreply.github.com>
2026-06-06 00:22:54 +0000

    fix: replace all CDMS V2 references with ChurchOS

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -15,7 +15,7 @@ export function register(config) {
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
         navigator.serviceWorker.ready.then(() => {
-          console.log('CDMS running in offline-ready mode (localhost).');
+          console.log('ChurchOS running in offline-ready mode (localhost).');
         });
       } else {
         registerValidSW(swUrl, config);
